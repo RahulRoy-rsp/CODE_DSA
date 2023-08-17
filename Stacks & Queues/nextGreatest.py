62d473a1b6acd4f47416a03119145504f474b942
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-08-17 17:53:21 +0530

    Next greatest element in an array

--- Stacks & Queues/nextGreatest.py
+++ Stacks & Queues/nextGreatest.py
@@ -1,5 +1,6 @@
 arr = [6, 3, 1, 5, 1]
 
+# Approach 1
 def nextGreatest(arr):
     result = [-1] * len(arr)
     st = []
@@ -10,6 +11,25 @@ def nextGreatest(arr):
             st.pop()
         st.append(i)
     
+    return result
+
+print(nextGreatest(arr))
+
+
+# Approach 2
+def nextGreatest(arr):
+    n = len(arr)
+    result = [-1] * n
+    st = []
+ 
+    for i in reversed(range(n)):
+        while st:
+            if st[-1] <= arr[i]:
+                st.pop()
+            else:
+                result[i] = st[-1]
+                break
+        st.append(arr[i])
     return result
 
 
