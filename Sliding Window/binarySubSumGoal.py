48a4989853bcc2bb91977e4ccbc48d75ab5df2f1
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-08-19 18:17:16 +0530

    number of substrings sum equal to goal

--- Sliding Window/binarySubSumGoal.py
+++ Sliding Window/binarySubSumGoal.py
@@ -1,6 +1,31 @@
 nums = [1,0,1,0,1]
 goal = 2
 
+# Approach 1
+def binarySubSum(nums, goal):
+    preSum = [0]
+    
+    for num in nums:
+        preSum.append(preSum[-1] + num)
+        
+    counts = 0
+    preSumCount = {}
+    
+    for i in preSum:
+        if i-goal in preSumCount:
+            counts += preSumCount[i-goal]
+            
+        if i in preSumCount:
+            preSumCount[i] += 1
+        else:
+            preSumCount[i] = 1
+    
+    return counts
+
+print(binarySubSum(nums, goal))
+
+
+# Approach 2
 def binarySubSum(nums, goal):
     counts = 0
     for i in range(len(nums)):
