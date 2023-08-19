64e0e774a00e2ee6bf40faf716de7f7f7ba16556
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-08-19 18:11:19 +0530

    number of substrings with a, b, c in it

--- Sliding Window/substringWithABC.py
+++ Sliding Window/substringWithABC.py
@@ -1,5 +1,28 @@
 s = "abcabca"
 
+
+# Approach 1
+def substringWithABC(s):
+    left = 0
+    ans = 0
+    counts = {'a': 0, 'b': 0, 'c': 0}
+    
+    for right in range(len(s)):
+        if s[right] in counts:
+            counts[s[right]] += 1
+            
+            while all(counts[c] > 0 for c in 'abc'):
+                if s[left] in counts:
+                    counts[s[left]] -= 1
+                
+                ans += len(s) - right
+                left += 1
+    return ans
+
+print(substringWithABC(s))
+
+
+# Approach 2
 def substringWithABC(s):
     left = 0
     ans = 0
